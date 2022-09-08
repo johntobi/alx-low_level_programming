d7d13a382b313915f3889db490704aa7050c67e9
johntobi <tobbex4jc@gmail.com>
2022-09-08 12:16:55 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
 */
 int main(void)
 {
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n, Anything");
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
 return (1);
 }
